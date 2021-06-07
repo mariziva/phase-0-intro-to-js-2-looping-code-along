0a5dcd58906416e3f1d777730d8bfd43504bb3b7
Mari Taksa <welch.mari@gmail.com>
2021-06-07 15:40:11 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+// Code your solutions in this fil
+function writeCards (name, event) {
+    let newArray = [];
+    for (let i=0; i < name.length; i++) {
+        newArray.push (`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    };
+    return newArray;
+};
+
+function countDown (i) {
+    while (i>=0) {
+        console.log(i);
+        i--;
+    }
+};
